23bf17afa74f24e031aa776ce3fca8f814e75316
Jordan <jordanrafael92@gmail.com>
2022-01-14 19:22:30 -0300

    Created domain model

--- backend/src/main/java/com/jordansilva/dsmovie/entities/User.java
+++ backend/src/main/java/com/jordansilva/dsmovie/entities/User.java
@@ -1,17 +1,35 @@
 package com.jordansilva.dsmovie.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String name;
+	private String email;
 	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public User() {
 		
 	}
 
-	public User(Long id, String name) {
+	public User(Long id, String email) {
 		this.id = id;
-		this.name = name;
+		this.email = email;
 	}
 
 	public Long getId() {
@@ -20,14 +38,6 @@ public class User {
 
 	public void setId(Long id) {
 		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 	
 }
