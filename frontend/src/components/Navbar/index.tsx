d23cf3113458022e4027465ff2340c137065d5c8
Jordan <jordanrafael92@gmail.com>
2022-01-20 21:04:32 -0300

    Alter title and colors

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -6,7 +6,7 @@ function Navbar() {
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
-                    <h1>Movie Rating</h1>
+                    <h1>Avaliação de Filmes</h1>
                     <a href="https://github.com/Jordanrafael92">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
